c61d16b56c1a3ca6596406566a4ffccfa5c56f80
shimozuki <robbishimozuki@gmail.com>
2024-06-24 15:10:26 +0800

    first commit

---
+++ resources/views/includes/landing/footer.blade.php
@@ -0,0 +1,7 @@
+  <!-- ======= Footer ======= -->
+  <footer id="footer">
+    <div class="container">
+        <div class="text-center">
+            © 2022 | By <strong>Go Trash</strong>
+        </div>
+  </footer><!-- End Footer -->
